1e75bdd2ac49fb4e7a1b71493c95a6f57424b854
nuomi1 <nuomi1@qq.com>
2024-05-26 23:24:49 +0800

    :tada: First commit.

---
+++ functions/omf-plugin.fish
@@ -0,0 +1,3 @@
+function omf-plugin -d "My package"
+  # Package entry-point
+end
---
+++ init.fish
@@ -0,0 +1,6 @@
+# omf-plugin initialization hook
+#
+# You can use the following variables in this file:
+# * $package       package name
+# * $path          package path
+# * $dependencies  package dependencies

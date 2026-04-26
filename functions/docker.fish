be09fd8e81491bfa047d62691f6ebab7dd59e334
nuomi1 <nuomi1@qq.com>
2026-04-26 10:21:35 +0800

    Refactor dpulla function to use docker image format

--- functions/docker.fish
+++ functions/docker.fish
@@ -1,5 +1,5 @@
 function dpulla -d "Update all non dangling images from a registry"
-    docker images | awk '(NR > 1) && ($2 !~ /none/) {print $1":"$2}' | sort | xargs -L1 docker pull
+    docker images --format '{{.Repository}}:{{.Tag}}' | grep -v '<none>' | sort | xargs -L1 docker pull
 end
 
 function drmia -d "Remove all dangling images"

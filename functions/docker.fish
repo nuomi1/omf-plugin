706596580fd17a94f69344a92a0aed7f0fda8afa
nuomi1 <nuomi1@qq.com>
2026-04-26 10:22:59 +0800

    Replace dangling image removal with docker image prune

--- functions/docker.fish
+++ functions/docker.fish
@@ -3,5 +3,5 @@ function dpulla -d "Update all non dangling images from a registry"
 end
 
 function drmia -d "Remove all dangling images"
-    docker images | awk '(NR > 1) && ($2 ~ /none/) {print $3}' | sort | xargs -L1 docker rmi
+    docker image prune -f
 end
